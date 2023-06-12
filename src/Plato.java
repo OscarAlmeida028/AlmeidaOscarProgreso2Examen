008088892eabc3660bd6a32885bf89a90d7a9b62
oscaralmeida028 <oscaralmeida028@gmail.com>
2023-06-12 16:10:13 -0500

    Se sube

--- src/Plato.java
+++ src/Plato.java
@@ -1,3 +1,11 @@
 public class Plato {
+    String nombrePlato;
+    float precio, calorias, tiempoPreparación;
 
+    public Plato(String nombrePlato, float precio, float calorias, float tiempoPreparación) {
+        this.nombrePlato = nombrePlato;
+        this.precio = precio;
+        this.calorias = calorias;
+        this.tiempoPreparación = tiempoPreparación;
+    }
 }
